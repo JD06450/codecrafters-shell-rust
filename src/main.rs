67407b7ef11f367b31559cd654289d7ec945a8b7
JD06450 <52086807+JD06450@users.noreply.github.com>
2024-08-03 19:33:40 -0400

    Add basic REPL loop

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+// use std::fmt;
+
+fn no_command_found(input: &String) -> String
+{
+	format!("{}: command not found", input)
+}
 
 fn main() {
-	// Uncomment this block to pass the first stage
-	print!("$ ");
-	io::stdout().flush().unwrap();
-
-	// Wait for user input
-	let stdin = io::stdin();
-	let mut input = String::new();
-	stdin.read_line(&mut input).unwrap();
-	if input.ends_with('\n') {
-		input.pop();
-	}
-
-	match input {
-		_ => println!("{}: command not found", input)
+	loop {
+		print!("$ ");
+		io::stdout().flush().unwrap();
+		
+		// Wait for user input
+		let stdin = io::stdin();
+		let mut input = String::new();
+		stdin.read_line(&mut input).unwrap();
+		if input.ends_with('\n') {
+			input.pop();
+		}
+		
+		let output: String;
+		
+		match input {
+			_ => output = no_command_found(&input)
+		}
+		println!("{}", output);
 	}
 }

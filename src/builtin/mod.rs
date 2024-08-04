9a6fc471ec9423e17c8ab68a519ebf8ae8868897
JD06450 <52086807+JD06450@users.noreply.github.com>
2024-08-04 12:44:01 -0400

    Add pwd builtin

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -14,24 +14,21 @@ pub fn get_env_path() -> Option<Vec<PathBuf>> {
 	}
 }
 
-pub fn exit(args: &Vec<String>) -> String
-{
+pub fn exit(args: &Vec<String>) -> String {
 	if args.len() < 2 {std::process::exit(0);}
 
 	let exit_code = args[1].parse::<i32>();
 	std::process::exit(exit_code.unwrap_or_default());
 }
 
-pub fn echo (args: &Vec<String>) -> String
-{
+pub fn echo(args: &Vec<String>) -> String {
 	let mut printed = args.clone();
 	printed.remove(0);
 
 	printed.join(" ")
 }
 
-pub fn cmd_type (args: &Vec<String>) -> String
-{
+pub fn cmd_type(args: &Vec<String>) -> String {
 	let str_to_find = &args[1];
 	for cmd in *COMMAND_NAMES {
 		if str_to_find == cmd {return format!("{} is a shell builtin", cmd);}
@@ -52,12 +49,17 @@ pub fn cmd_type (args: &Vec<String>) -> String
 	format!("{}: not found", str_to_find)
 }
 
-pub static COMMANDS: [(&'static str, fn(&Vec<String>) -> String); 3] = [
+pub fn pwd(_args: &Vec<String>) -> String {
+	env::current_dir().unwrap().display().to_string()
+}
+
+pub static COMMANDS: [(&'static str, fn(&Vec<String>) -> String); 4] = [
 	("exit", exit),
 	("echo", echo),
-	("type", cmd_type)
+	("type", cmd_type),
+	("pwd", pwd)
 ];
 
 lazy_static! {
-	pub static ref COMMAND_NAMES: [&'static str; 3] = COMMANDS.map(|c| c.0);
+	pub static ref COMMAND_NAMES: [&'static str; 4] = COMMANDS.map(|c| c.0);
 }

6867aab622aa41723c774e5773573b46b53fcb69
JD06450 <52086807+JD06450@users.noreply.github.com>
2024-08-03 20:50:07 -0400

    Add echo command

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -10,8 +10,17 @@ pub fn exit(args: &Vec<String>) -> String
 	std::process::exit(exit_code.unwrap_or_default());
 }
 
+pub fn echo (args: &Vec<String>) -> String
+{
+	let mut printed = args.clone();
+	printed.remove(0);
+
+	printed.join(" ")
+}
 
 
-pub static COMMANDS: [(&'static str, fn(&Vec<String>) -> String); 1] = [
-	("exit", exit)
+
+pub static COMMANDS: [(&'static str, fn(&Vec<String>) -> String); 2] = [
+	("exit", exit),
+	("echo", echo)
 ];
